7f31b63be272bfe7ae7f7f94e75445888e309c42
Christophe Soares <christophe.p.soares@mindera.com>
2022-11-21 15:31:08 +0000

    Fix index message with the © 2022

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,5 @@ Laravel Programming Lab. | Index Page
 @endsection
 
 @section('content')
-<h2 class="mt-5 mb-3 text-center">Laravel Programming Lab.!</h2>
+<h2 class="mt-5 mb-3 text-center">Laravel Programming Lab. © 2022 !</h2>
 @endsection
